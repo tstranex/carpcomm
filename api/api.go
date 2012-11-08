47c392e1940d25edf8b97f0c42bca96b04914bea
Timothy Stranex <timothy@stranex.com>
2012-11-08 20:15:49 +0100

    Add PostPacket call and more comments.

--- api/api.go
+++ api/api.go
@@ -2,7 +2,24 @@
 // Copyright 2012 Carpcomm GmbH
 
 // API for the Carpcomm network.
-
+// BETA
+//
+// Examples:
+//
+// Initialize the client:
+//
+//   c, err := api.NewAPIClient("your_station_id", "your_station_secret")
+//
+// Fetch some packets:
+//
+//   packets, err := c.GetLatestPackets("your_satellite_id", 3)
+//
+// Post a packet:
+//
+//   var p api.Packet
+//   p.Timestamp = time.Now().Unix()
+//   p.Frame = make([]byte, 100)
+//   err := c.PostPacket("your_satellite_id", p)
 package api
 
 import "crypto/tls"
@@ -13,8 +30,10 @@ import "encoding/base64"
 import "encoding/json"
 import "fmt"
 import "errors"
+import "bytes"
 
 const defaultApiHost = "api.carpcomm.com:5051"
+const jsonMimeType = "application/json"
 
 type APIClient struct {
 	host string
@@ -49,9 +68,57 @@ func NewAPIClient(station_id, station_secret string) (*APIClient, error) {
 
 
 type Packet struct {
-	Timestamp int64
+	Timestamp int64  // Unix timestamp: UTC seconds since the epoch.
 	Frame []byte
 }
+
+
+type postPacketRequest struct {
+	StationId string `json:"station_id"`
+	StationSecret string `json:"station_secret"`
+	Timestamp int64 `json:"timestamp"`
+	SatelliteId string `json:"satellite_id"`
+	Format string `json:"format"`
+	FrameBase64 string `json:"frame_base64"`
+}
+
+
+// Post a packet that was received from the given satellite.
+func (c *APIClient) PostPacket(satellite_id string, p Packet) error {
+	var req postPacketRequest
+	req.StationId = c.station_id
+	req.StationSecret = c.station_secret
+	req.Timestamp = p.Timestamp
+	req.SatelliteId = satellite_id
+	req.Format = "FRAME"
+	req.FrameBase64 = base64.StdEncoding.EncodeToString(p.Frame)
+	body, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	var u url.URL
+	u.Scheme = "https"
+	u.Host = c.host
+	u.Path = "/PostPacket"
+
+	fmt.Printf("url: %s\n", u.String())
+	fmt.Printf("body: %s\n", body)
+
+	resp, err := c.client.Post(
+		u.String(), jsonMimeType, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		text, _ := ioutil.ReadAll(resp.Body)
+		return errors.New(fmt.Sprintf(
+			"HTTP error: %s: %s", resp.Status, (string)(text)))
+	}
+
+	return nil
+}
+
 
 type packet struct {
 	Timestamp int64 `json:"timestamp"`
@@ -60,6 +127,8 @@ type packet struct {
 
 // Fetch packets for the given satellite from the server.
 // At most 'limit' packets will be returned.
+// Note that you can only receive packets from satellites that you are
+// authorized to read.
 func (c *APIClient) GetLatestPackets(satellite_id string, limit int) (
 	[]Packet, error) {
 
